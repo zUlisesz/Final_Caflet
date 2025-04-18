b24b46d23f66ee2449a1f64cbfeb4405ee521997
zUlisez <zperplexz@gmail.com>
2025-04-18 17:36:09 -0600

    updated

---
+++ views/custom_controls.py
@@ -0,0 +1,46 @@
+import flet as ft
+
+class Box700x340(ft.Container):
+    def __init__(self, height= None , control = None, event = None, padding = None):
+        super().__init__(
+            width= 700,
+            height= 340 if height == None else height,
+            content= control,
+            border_radius= 12,
+            padding = padding,
+            alignment= ft.alignment.center,
+            shadow= ft.BoxShadow(
+                blur_radius= 10,
+                color = ft.Colors.BLACK87,
+                offset= ft.Offset(4, 4),
+            ),
+            gradient= ft.LinearGradient(
+                begin= ft.alignment.top_right,
+                end= ft.alignment.bottom_left,
+                colors=[ft.Colors.DEEP_ORANGE_ACCENT, ft.Colors.BLACK12],
+            )
+        )
+        
+class Box1460x345(ft.Container):
+    def __init__(self, controls: list, height = None):
+        super().__init__(
+            width = 1460 ,
+            height= 345 if height == None else height,
+            padding = ft.padding.symmetric( vertical= 10 , horizontal=20 ) , 
+            content= ft.Row(
+                controls= controls
+            )
+        )
+        
+class Box400x500(ft.Container):
+    def __init__(self, content = None, event = None):
+        super().__init__(
+            width= 440 ,
+            height= 500,
+            border_radius= 12,
+            alignment= ft.alignment.center,
+            content= content, 
+            ink= True, 
+            on_click= event
+        )
+        
